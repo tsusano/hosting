0822720a0d0c663d87df16353500dffc8e4a10f5
tsusano <chbindu2102@gmail.com>
2024-04-11 06:44:12 +0530

    update setting

--- projectapp/settings.py
+++ projectapp/settings.py
@@ -34,7 +34,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,7 +80,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "projectapp.wsgi.app"
+WSGI_APPLICATION = "projectapp.wsgi.application"
 
 
 # Database
