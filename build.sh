fd6fdf070873f209d7356c8e5618f3ef5012f074
tsusano <chbindu2102@gmail.com>
2024-04-11 07:29:05 +0530

    changes

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+#Build the project
+echo "Building the project..."
+python3.9 -m pip3 install -r requirements.txt
+
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3.9 manage.py collectstatic --noinput
